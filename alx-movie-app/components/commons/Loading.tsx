0f17875300a0d9290495f584d9215ac4d0cf12d5
OnlineElite <online.0.elite@gmail.com>
2025-08-10 18:10:28 +0100

    sitting up movies page component and Api fetching

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -1,13 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const Loading: React.FC=()=>{
-
-    return(
-        <div>
-            <h1><FontAwesomeIcon icon={faGithub} /> Hello Loading</h1>
-        </div>
-    )
-}
+const Loading: React.FC = () => {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-6xl font-semibold text-white mb-4 animate-pulse">
+          Loading...
+        </h1>
+        <p className="text-lg text-gray-300">Please wait, we&apos;re getting next set of movies ready for you.</p>
+      </div>
+    </div>
+  );
+};
 
 export default Loading;
--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    domains: ["m.media-amazon.com"], // allow Amazon image URLs
+  },
   reactStrictMode: true,
 };
 
